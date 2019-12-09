8ff92cfe9370c198b0ad30791c3f74346fda1669
Thorgeirbjornsson <bjornssonthorgeir@gmail.com>
2019-12-09 18:01:04 +0000

    check unused styles

---
+++ src/components/ShowTime/ShowTime.styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import Colors from '../../resources/Colors';
+
+export default StyleSheet.create({
+
+    buyNow: {
+        fontSize: 14,
+        color: Colors.white,
+        paddingVertical: 5,
+    },
+});
--- src/screens/UpcomingMovies/UpcomingMovies.styles.js
+++ src/screens/UpcomingMovies/UpcomingMovies.styles.js
@@ -1,43 +1,43 @@
 import { StyleSheet } from 'react-native';
-import Colors from '../../resources/Colors';
+// import Colors from '../../resources/Colors';
 
 
 export default StyleSheet.create({
-    cinemaMeta: {
-        flex: 1,
-        justifyContent: 'space-between',
-        paddingHorizontal: 15,
-        backgroundColor: Colors.charcoal,
-        borderRadius: 15,
-        marginBottom: 15,
-    },
-    header: {
-        height: 50,
-        justifyContent: 'center',
-    },
-    headerText: {
-        fontSize: 20,
-        color: Colors.white,
-    },
-    description: {
-        color: Colors.gray,
-        fontSize: 12,
-        lineHeight: 16,
-    },
-    footer: {
-        height: 50,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    footerText: {
-        fontSize: 12,
-        color: Colors.gray,
-    },
-    movieList: {
-        flex: 1,
-        padding: 15,
-        backgroundColor: Colors.charcoal,
-        borderRadius: 15,
-    },
+    // cinemaMeta: {
+    //     flex: 1,
+    //     justifyContent: 'space-between',
+    //     paddingHorizontal: 15,
+    //     backgroundColor: Colors.charcoal,
+    //     borderRadius: 15,
+    //     marginBottom: 15,
+    // },
+    // header: {
+    //     height: 50,
+    //     justifyContent: 'center',
+    // },
+    // headerText: {
+    //     fontSize: 20,
+    //     color: Colors.white,
+    // },
+    // description: {
+    //     color: Colors.gray,
+    //     fontSize: 12,
+    //     lineHeight: 16,
+    // },
+    // footer: {
+    //     height: 50,
+    //     flexDirection: 'row',
+    //     justifyContent: 'space-between',
+    //     alignItems: 'center',
+    // },
+    // footerText: {
+    //     fontSize: 12,
+    //     color: Colors.gray,
+    // },
+    // movieList: {
+    //     flex: 1,
+    //     padding: 15,
+    //     backgroundColor: Colors.charcoal,
+    //     borderRadius: 15,
+    // },
 });
