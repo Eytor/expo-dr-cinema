638cc53f455c8d887110fb91aed75367d60a3134
eytor <eythoras16@ru.is>
2019-12-09 13:08:35 +0000

    added service for upcoming movies

--- src/service/services.js
+++ src/service/services.js
@@ -54,3 +54,21 @@ export function getAllCinemas(token) {
         throw new Error('error ', response);
     }).catch((error) => { console.log(error); });
 }
+
+export function getUpcomingMovies(token) {
+    const url = 'http://api.kvikmyndir.is/upcoming';
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'x-access-token': token,
+            'Accept-Charset': 'utf-8',
+        },
+    }).then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+            return response.json();
+        }
+        throw new Error('error ', response);
+    }).catch((error) => { console.log(error); });
+}
