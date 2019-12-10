dc465277dae2eab9bef1c8ec74b38d8792fc469c
eytor <eythoras16@ru.is>
2019-12-10 16:02:51 +0000

    changed UpcomingMovieDetails to stateless Component

--- src/screens/UpcomingMovieDetails/UpcomingMovieDetails.js
+++ src/screens/UpcomingMovieDetails/UpcomingMovieDetails.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
     View,
     Text,
@@ -12,56 +12,51 @@ import { WebView } from 'react-native-webview';
 import defaultStyles from '../../resources/defaultStyles';
 import styles from './UpcomingMovieDetails.styles';
 
-class MovieDetailScreen extends Component {
-    render() {
-        const { movie } = this.props;
-        const dimensions = Dimensions.get('window');
-        const imageWidth = (dimensions.width - 30);
-        const imageHeight = (dimensions.height) - 95;
-        const trailers = movie.trailers.map((trailer, index) => (
-            <View key={index}>
-                <Text style={styles.headerText}>{`${trailer.site}: ${trailer.name}`}</Text>
-                <WebView
-                    style={{ height: 200, width: '100%' }}
-                    javaScriptEnabled
-                    source={{ uri: trailer.url }}
-                />
-            </View>
-        ));
-        return (
-            <ScrollView style={{ flex: 1 }}>
-                <View style={[defaultStyles.container, { paddingHorizontal: 15 }]}>
-                    <View style={styles.movieWrapper}>
-                        <Image
-                            style={[styles.moviePoster, { width: imageWidth, height: imageHeight }]}
-                            resizeMode="cover"
-                            source={{ uri: movie.poster }}
-                        />
-                        <View style={{ padding: 15 }}>
-                            <View style={styles.header}>
-                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                    <Text style={styles.headerText}>{movie.name}</Text>
-                                </View>
-                            </View>
-                            <View style={styles.body}>
-                                <Text style={styles.plotText}>
-                                    {movie.plot.replace(/\n/g, '')}
-                                </Text>
+const UpcomingMovieDetailScreen = ({ movie }) => {
+    const dimensions = Dimensions.get('window');
+    const imageWidth = (dimensions.width - 30);
+    const imageHeight = (dimensions.height) - 95;
+    const trailers = movie.trailers.map((trailer, index) => (
+        <View key={index}>
+            <Text style={styles.headerText}>{`${trailer.site}: ${trailer.name}`}</Text>
+            <WebView
+                style={{ height: 200, width: '100%' }}
+                javaScriptEnabled
+                source={{ uri: trailer.url }}
+            />
+        </View>
+    ));
+    return (
+        <ScrollView style={{ flex: 1 }}>
+            <View style={[defaultStyles.container, { paddingHorizontal: 15 }]}>
+                <View style={styles.movieWrapper}>
+                    <Image
+                        style={[styles.moviePoster, { width: imageWidth, height: imageHeight }]}
+                        resizeMode="cover"
+                        source={{ uri: movie.poster }}
+                    />
+                    <View style={{ padding: 15 }}>
+                        <View style={styles.header}>
+                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                <Text style={styles.headerText}>{movie.name}</Text>
                             </View>
                         </View>
-                        <View>
-                            {trailers}
+                        <View style={styles.body}>
+                            <Text style={styles.plotText}>
+                                {movie.plot.replace(/\n/g, '')}
+                            </Text>
                         </View>
                     </View>
+                    <View>
+                        {trailers}
+                    </View>
                 </View>
-            </ScrollView>
-        );
-    }
-}
+            </View>
+        </ScrollView>
+    );
+};
 
-MovieDetailScreen.propTypes = {
-    // navigation: PropTypes.object.isRequired,
-    // token: PropTypes.string.isRequired,
+UpcomingMovieDetailScreen.propTypes = {
     movie: PropTypes.shape({
         id: PropTypes.number.isRequired,
         name: PropTypes.string.isRequired,
@@ -76,4 +71,4 @@ const mapStateToProps = (props) => ({
     movie: props.movie,
 });
 
-export default connect(mapStateToProps)(MovieDetailScreen);
+export default connect(mapStateToProps)(UpcomingMovieDetailScreen);
